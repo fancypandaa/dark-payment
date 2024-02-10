84ea36b95f3474a9f2f1e4070c533d043e939bcd
liosub <mlio64565@gmail.com>
2024-02-10 20:59:40 +0000

    create plan repository interface

---
+++ src/main/java/com/bank/payment/repositories/PlanRepository.java
@@ -0,0 +1,9 @@
+package com.bank.payment.repositories;
+
+import com.bank.payment.domain.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan,Long> {
+}
