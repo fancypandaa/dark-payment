8f4f87c5453ffd14a6c7ce80b450269de7c17b72
liosub <mlio64565@gmail.com>
2024-02-10 20:59:19 +0000

    add plan mapper DTO test

---
+++ src/test/java/com/bank/payment/api/mapper/PlanMapperTest.java
@@ -0,0 +1,49 @@
+package com.bank.payment.api.mapper;
+
+import com.bank.payment.api.model.PlanDTO;
+import com.bank.payment.domain.BalanceTypes;
+import com.bank.payment.domain.Plan;
+import jakarta.persistence.Enumerated;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanMapperTest {
+    private static final Long ID= 2L;
+    private static final String NAME="PLAN-A";
+    private static final Long MINIMUM=1000L;
+    private static final Long MAXIMUM=50000L;
+    private static final BalanceTypes TYPE=BalanceTypes.POINTS;
+    private static final double INTEREST= .05;
+    private static final double FEES= 0.2;
+    PlanMapper planMapper = PlanMapper.INSTANCE;
+
+    @Test
+    void planToPlanDTO() {
+        Plan plan = new Plan();
+        plan.setId(ID); plan.setName(NAME);
+        plan.setType(TYPE);
+        plan.setMinimum(MINIMUM); plan.setMaximum(MAXIMUM);
+        plan.setFees(FEES); plan.setInterest(INTEREST);
+        PlanDTO planDTO = planMapper.planToPlanDTO(plan);
+        assertNotNull(planDTO);
+        assertEquals(ID,planDTO.getId());
+        assertEquals(NAME,planDTO.getName());
+        assertEquals(TYPE,planDTO.getType());
+    }
+
+    @Test
+    void planDtoToPlan() {
+
+        PlanDTO planDto = new PlanDTO();
+        planDto.setId(ID); planDto.setName(NAME);
+        planDto.setType(TYPE);
+        planDto.setMinimum(MINIMUM); planDto.setMaximum(MAXIMUM);
+        planDto.setFees(FEES); planDto.setInterest(INTEREST);
+        Plan plan = planMapper.planDtoToPlan(planDto);
+        assertNotNull(plan);
+        assertEquals(ID,plan.getId());
+        assertEquals(MINIMUM,plan.getMinimum());
+        assertEquals(MAXIMUM,plan.getMaximum());
+    }
+}
