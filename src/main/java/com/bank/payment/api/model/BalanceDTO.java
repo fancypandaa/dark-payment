95604d1adb9fa672a85b11efee77ad99a1b3cba9
liosub <mlio64565@gmail.com>
2024-02-10 20:48:53 +0000

    create balance DTO mapper

---
+++ src/main/java/com/bank/payment/api/model/BalanceDTO.java
@@ -0,0 +1,20 @@
+package com.bank.payment.api.model;
+
+import com.bank.payment.domain.Account;
+import com.bank.payment.domain.BalanceLogs;
+import com.bank.payment.domain.BalanceTypes;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+@Data
+public class BalanceDTO {
+    private Long id;
+    private BalanceTypes balanceTypes;
+    private double localBalance;
+    private double foreignBalance;
+    private Long totalCoins;
+    private double totalPoints;
+    private String Balance_State;
+    private ArrayList<BalanceLogs> balanceLog;
+}
