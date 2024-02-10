d2f8fc1400a2ded5ac0af0d05d0f309256984038
liosub <mlio64565@gmail.com>
2024-02-10 20:58:58 +0000

    create plan DTO mapper

---
+++ src/main/java/com/bank/payment/api/model/PlanDTO.java
@@ -0,0 +1,18 @@
+package com.bank.payment.api.model;
+
+import com.bank.payment.domain.BalanceTypes;
+import jakarta.persistence.Enumerated;
+import lombok.Data;
+
+@Data
+public class PlanDTO {
+    private Long Id;
+    private String name;
+    private Long minimum;
+    private Long maximum;
+    @Enumerated
+    private BalanceTypes type;
+    private double interest;
+    private double fees;
+
+}
