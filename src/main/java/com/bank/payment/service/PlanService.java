216c8017c32e6d12ddf91a7841fcc87c062de9c5
liosub <mlio64565@gmail.com>
2024-02-10 20:59:51 +0000

    create plan service interface

---
+++ src/main/java/com/bank/payment/service/PlanService.java
@@ -0,0 +1,11 @@
+package com.bank.payment.service;
+
+import com.bank.payment.api.model.PlanDTO;
+
+public interface PlanService {
+    PlanDTO getPlansById(Long id);
+    PlanDTO createNewPlan(PlanDTO planDTO);
+    PlanDTO savePlanDTOByDTO(Long id,PlanDTO planDTO);
+    PlanDTO patchAccount(Long id, PlanDTO planDTO);
+    void deletePlanDTOById(Long id);
+}
