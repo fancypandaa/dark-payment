3fa5f6ee3853c411a4048a649779aa0a19e1791f
liosub <mlio64565@gmail.com>
2024-02-21 22:59:47 +0000

    change test

--- src/main/java/com/bank/payment/PaymentApplication.java
+++ src/main/java/com/bank/payment/PaymentApplication.java
@@ -1,17 +1,17 @@
 package com.bank.payment;
 
+import com.bank.payment.auctions.subscriber.AuctionsEventListener;
 import com.bank.payment.concurrencyFactory.BalanceFactory;
 import com.bank.payment.concurrencyFactory.BalanceOptions;
 import com.bank.payment.config.RsaKeyConfigProperties;
 
 import com.bank.payment.domain.BalanceTypes;
-import com.bank.payment.domain.User;
-import com.bank.payment.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.data.redis.connection.DefaultMessage;
+import org.springframework.data.redis.connection.Message;
 
 import java.math.BigDecimal;
 
@@ -23,12 +23,12 @@ public class PaymentApplication {
 		SpringApplication.run(PaymentApplication.class, args);
 
 		BalanceOptions balanceOptions= new BalanceOptions();
-		BalanceFactory balanceFactory=balanceOptions.createBalance("points",0.17, BalanceTypes.FOREIGN_CURRENCY);
+		BalanceFactory balanceFactory=balanceOptions.createBalance("localCurrency",0.17, BalanceTypes.FOREIGN_CURRENCY);
 		System.out.println(balanceFactory.getAvailableToUseAfterTax(BigDecimal.valueOf(5000)));
 		System.out.println(balanceFactory.getAvailableToUseAfterTax(BigDecimal.valueOf(5000)));
-
 
 	}
+
 //	@Bean
 //	public CommandLineRunner initializeUser(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
 //		System.out.println("XXX");
