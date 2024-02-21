72829c20784c74e0f4ecdc9713e9cad65c961890
liosub <mlio64565@gmail.com>
2024-02-21 22:59:23 +0000

    create endpoint for publish method

--- src/main/java/com/bank/payment/auctions/publisher/AuctionController.java
+++ src/main/java/com/bank/payment/auctions/publisher/AuctionController.java
@@ -1,2 +1,24 @@
-package com.bank.payment.auctions.publisher;public class AuctionController {
+package com.bank.payment.auctions.publisher;
+
+import com.bank.payment.auctions.subscriber.AuctionModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(AuctionController.BASE_URL)
+public class AuctionController {
+    public static final String BASE_URL = "/api/auction";
+
+    @Autowired
+    private AuctionService auctionService;
+
+    @PostMapping("/publish")
+    public String publish(@RequestBody AuctionModel auctionModel){
+        auctionService.publish(auctionModel);
+        return "Success";
+    }
 }
