5edd8071efe73053f2ce3845da55000d97e8a5ba
liosub <mlio64565@gmail.com>
2024-02-21 22:54:34 +0000

    create redis container and message listeners configuration beans

--- src/main/java/com/bank/payment/config/RedisSubscriberConfig.java
+++ src/main/java/com/bank/payment/config/RedisSubscriberConfig.java
@@ -1,2 +1,29 @@
-package com.bank.payment.config;public class RedisSubscriberConfig {
+package com.bank.payment.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+@AllArgsConstructor
+public class RedisSubscriberConfig {
+    private final MessageListener messageListener;
+    private final RedisConnectionFactory redisConnectionFactory;
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter(){
+        return new MessageListenerAdapter(messageListener);
+    }
+    @Bean
+    public RedisMessageListenerContainer redisContainer(ChannelTopic channelTopic){
+        RedisMessageListenerContainer container =new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(messageListener,channelTopic);
+        System.out.println(messageListener+" XXX "+channelTopic);
+        return container;
+    }
 }
