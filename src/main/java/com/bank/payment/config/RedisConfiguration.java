863b55c2c05a65552af8ea8abc6798f0f04d93e9
liosub <mlio64565@gmail.com>
2024-02-21 22:52:36 +0000

    add redis template and connection beans

--- src/main/java/com/bank/payment/config/RedisConfiguration.java
+++ src/main/java/com/bank/payment/config/RedisConfiguration.java
@@ -1,2 +1,29 @@
-package com.bank.payment.config;public class RedisConfiguration {
+package com.bank.payment.config;
+
+import com.bank.payment.auctions.subscriber.AuctionsEventListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${redis.pub_sub.topic:channel-events}")
+    private String topic;
+    @Bean
+    public ChannelTopic channelTopic(){
+        return new ChannelTopic(topic);
+    }
+    @Bean(name = "pub_subRedisTemplate")
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
 }
