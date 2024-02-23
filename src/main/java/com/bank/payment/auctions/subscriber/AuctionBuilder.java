badae2a1ea6c4e8bb8b416803a79c565d0c63ded
liosub <mlio64565@gmail.com>
2024-02-23 19:20:58 +0000

    add auction builder for create and manage auctions

--- src/main/java/com/bank/payment/auctions/subscriber/AuctionBuilder.java
+++ src/main/java/com/bank/payment/auctions/subscriber/AuctionBuilder.java
@@ -1,2 +1,33 @@
-package com.bank.payment.auctions.subscriber;public class AuctionBuilder {
+package com.bank.payment.auctions.subscriber;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public abstract class AuctionBuilder {
+    LinkedList<AuctionModel> ll;
+
+    public AuctionBuilder(LinkedList<AuctionModel> ll) {
+        this.ll = ll;
+    }
+
+    void createAuction(AuctionModel auctionModel){
+        this.ll.add(auctionModel);
+        AuctionFactory.getAuction(auctionModel);
+    }
+     void closeAuction(AuctionModel auctionModel,Optional<String> voterId){
+        this.ll.remove(auctionModel);
+        if(voterId.isPresent()){
+            AuctionFactory.closeAuction(auctionModel.getAuctionId(),voterId.get());
+        }
+        else{
+            AuctionFactory.removeAuction(auctionModel.getAuctionId());
+        }
+     }
+     void extendAuction(String auctionId){
+        AuctionFactory.extendAuction(auctionId);
+     }
+     void addVote(String auctionId,String voterId, BigDecimal offer){
+        AuctionFactory.addVote(auctionId,voterId,offer);
+     }
+
 }
