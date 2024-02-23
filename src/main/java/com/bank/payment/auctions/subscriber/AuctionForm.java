9ccf70bfb3ee26a3acba09ca400b7c8fc82c54d5
liosub <mlio64565@gmail.com>
2024-02-23 19:21:48 +0000

    add auction form for very contains all info about auction

--- src/main/java/com/bank/payment/auctions/subscriber/AuctionForm.java
+++ src/main/java/com/bank/payment/auctions/subscriber/AuctionForm.java
@@ -1,2 +1,20 @@
-package com.bank.payment.auctions.subscriber;public class AuctionForm {
+package com.bank.payment.auctions.subscriber;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuctionForm {
+    private String auctionId;
+    private String state;
+    private Integer voted;
+    private Double cost;
+    private Date openAt;
+    private Date closeAt;
+    private HashMap<String, BigDecimal> buyers= new HashMap<>();
+
 }
