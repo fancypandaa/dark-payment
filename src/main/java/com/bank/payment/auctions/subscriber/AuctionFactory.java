5a7065f5c380bb7a1c2e125e6735843413aedfe4
liosub <mlio64565@gmail.com>
2024-02-25 18:16:01 +0000

    add append method to default buyer map

--- src/main/java/com/bank/payment/auctions/subscriber/AuctionFactory.java
+++ src/main/java/com/bank/payment/auctions/subscriber/AuctionFactory.java
@@ -1,20 +1,26 @@
 package com.bank.payment.auctions.subscriber;
 
+import org.springframework.stereotype.Component;
+
 import java.math.BigDecimal;
 import java.util.*;
+@Component
 public class AuctionFactory {
     private static Map<String, AuctionForm> auctionList = new HashMap<>();
     public static AuctionForm getAuction(AuctionModel auctionModel){
-        AuctionForm result=auctionList.get(auctionModel.getAuctionId());
-        if(result == null){
+        Optional<AuctionForm> result=Optional.ofNullable(auctionList.get(auctionModel.getAuctionId()));
+        if(result.isEmpty()){
             AuctionForm auctionForm= new AuctionForm();
             auctionForm.setAuctionId(auctionModel.getAuctionId());
             auctionForm.setState("open");
             auctionForm.setOpenAt(new Date());
             auctionForm.setCloseAt(auctionForm.getOpenAt());
             auctionList.put(auctionModel.getAuctionId(),auctionForm);
+            result= Optional.of(auctionForm);
+
         }
-        return result;
+        System.out.println("MAP SIZE:: "+ auctionList.size() +" "+result.toString());
+        return result.get();
     }
 
     public static void removeAuction(String auctionId){
@@ -32,7 +38,9 @@ public class AuctionFactory {
     public static void addVote(String auctionId,String voterId, BigDecimal offer){
         AuctionForm auctionForm = auctionList.get(auctionId);
         auctionForm.setVoted(auctionForm.getVoted()+1);
-        auctionForm.getBuyers().put(voterId,offer);
+        auctionForm.addBuyers(voterId,offer);
+        System.out.println("xxxx");
+        System.out.println(auctionForm.getBuyers().get(voterId)+" <<<<<<<<");
     }
     public static BigDecimal closeAuction(String auctionId,String voterId){
         AuctionForm auctionForm = auctionList.get(auctionId);
