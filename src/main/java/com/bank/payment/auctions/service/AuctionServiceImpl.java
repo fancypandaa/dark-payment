c171446906b9259a5cc141e4c78f3803205da8a0
liosub <mlio64565@gmail.com>
2024-03-06 17:34:00 +0000

    add auctionForm curd in redis methods

--- src/main/java/com/bank/payment/auctions/service/AuctionServiceImpl.java
+++ src/main/java/com/bank/payment/auctions/service/AuctionServiceImpl.java
@@ -1,6 +1,8 @@
 package com.bank.payment.auctions.service;
 
+import com.bank.payment.auctions.domain.AuctionForm;
 import com.bank.payment.auctions.domain.AuctionModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,15 +20,53 @@ public class AuctionServiceImpl implements AuctionService{
     @Autowired
     @Qualifier("pub_subRedisTemplate")
     private RedisTemplate<String,Object> redisTemplate;
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @Autowired
     private ChannelTopic channelTopic;
 
     @Override
     public Long publish(AuctionModel auctionModel) {
-        UUID uuid= UUID.randomUUID();
-        auctionModel.setAuctionId(uuid.toString());
-        auctionModel.setCreatedAt((Instant.now()).toString());
-        return redisTemplate.convertAndSend(channelTopic.getTopic(),auctionModel);
+        try{
+            UUID uuid= UUID.randomUUID();
+            auctionModel.setAuctionId(uuid.toString());
+            auctionModel.setCreatedAt((Instant.now()).toString());
+            return redisTemplate.convertAndSend(channelTopic.getTopic(),auctionModel);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
     }
+
+    @Override
+    public void putAuctionForm(String auctionId,AuctionForm auctionForm) {
+        try {
+            redisTemplate.opsForHash().put(auctionId,AuctionForm.class.getFields(),auctionForm);
+        }
+        catch (RuntimeException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public AuctionForm getAuctionForm(String auctionId) {
+        try {
+            Object o= redisTemplate.opsForHash().get(auctionId,AuctionForm.class.getFields());
+            if(o == null) return null;
+            AuctionForm auctionForm =objectMapper.convertValue(o,AuctionForm.class);
+            return auctionForm;
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteAuctionForm(String auctionId) {
+        redisTemplate.opsForHash().delete(auctionId);
+    }
+
+
 }
